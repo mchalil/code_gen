d9becf091403c1f811518dd7f28c971d49fa1212
mchalil <mohammed.chalil@outlook.com>
2017-02-22 23:20:15 +0530

    minor changes

--- v1/algorithm/src/filters/cic.cpp
+++ v1/algorithm/src/filters/cic.cpp
@@ -3,6 +3,7 @@
 
 #include "cic.h"
 
+#define INITIAL_SHIFT (0)
 #define ENABLE_PRUNING 
 
 cic::cic(Integer BlockSz, Integer NN, Integer RR, Integer MM)
@@ -19,28 +20,28 @@ cic::cic(Integer BlockSz, Integer NN, Integer RR, Integer MM)
 #ifdef ENABLE_PRUNING
 	if (RR < 16)
 	{   // pY[countOut++] = delayBuff[N] >> 4; // r <= 16
-		pruning[0] = 1;
+		pruning[0] = INITIAL_SHIFT;
 		pruning[1] = 4;
 	}
 	else if (RR < 20)
 	{ // pY[countOut++] = delayBuff[N] >> 6;  // r = 20
-		pruning[0] = 1;
+		pruning[0] = INITIAL_SHIFT;
 		pruning[1] = 6;
 	}
 	else if (RR < 30) 
 	{ // pY[countOut++] = delayBuff[N] >> 10;  // r = 30
-		pruning[0] = 1;
+		pruning[0] = INITIAL_SHIFT;
 		pruning[1] = 10;
 
 	}
 	else if (RR < 40)
 	{ // pY[countOut++] = delayBuff[N] >> 14;  // r = 40
-		pruning[0] = 1;
+		pruning[0] = INITIAL_SHIFT;
 		pruning[1] = 14;
 	}
 	else
 	{ //pY[countOut++] = delayBuff[N] >> 18;  // r = 64
-		pruning[0] = 1;
+		pruning[0] = INITIAL_SHIFT;
 		pruning[1] = 18;
 	}
 #endif // ENABLE_PRUNING
@@ -55,7 +56,7 @@ IndexInt cic::process(tFract32 *pX, tFract32 *pY)
 	IndexInt countOut = 0;
 	for (IndexInt i = 0; i < BlockSize; i++)
 	{
-		State[1] += pX[i]>> pruning[0];
+		State[1] += pX[i]; //  >> pruning[0];
 #if 1
 		for (IndexInt j = 1; j < N; j++) 
 		{
@@ -76,16 +77,17 @@ IndexInt cic::process(tFract32 *pX, tFract32 *pY)
 		{
 			delayBuff[1] = State[N - 1]; // inp to the first comb
 
-			for (IndexInt j = 1; j <= N; j++) {
-				// using  matlab indexing. j = 0 is unused
+			for (IndexInt j = 1; j <= N; j++) {	// using  matlab indexing. j = 0 is unused
 				delayBuff[j + 1] = delayBuff[j] - compOut[j];
 				compOut[j] = delayBuff[j];
 			}
+#if 0
 			// pY[countOut++] = delayBuff[N] >> 4; // r <= 16
 			// pY[countOut++] = delayBuff[N] >> 6;  // r = 20
 			// pY[countOut++] = delayBuff[N] >> 10;  // r = 30
 			// pY[countOut++] = delayBuff[N] >> 14;  // r = 40
 			// pY[countOut++] = delayBuff[N] >> 18;  // r = 64
+#endif
 			pY[countOut++] = delayBuff[N] >> pruning[1];
 		}
 	}
